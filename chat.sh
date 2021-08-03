967457725407ec33d84522be3230c261aeef241a
Briggsdf <briggsdf@gmail.com>
2021-08-02 20:49:20 -0400

    Update chat.sh

--- chat.sh
+++ chat.sh
@@ -1,11 +1,12 @@
-apt upgrade -y
-apt install mongodb
-curl -sL https://deb.nodesource.com/setup_8.x | bash -
-apt install nodejs curl graphicsmagick -y
-npm install -g n
-cd /opt
-curl -L https://rocket.chat/releases/latest/download -o rocket.chat.tgz
-tar -zxvf rocket.chat.tgz
-mv bundle Rocket.Chat
-cd Rocket.Chat/programs/server
-npm install
+sudo apt-get -y update
+sudo apt-get install -y dirmngr gnupg && sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
+echo "deb http://repo.mongodb.org/apt/debian stretch/mongodb-org/4.0 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
+sudo apt-get -y update && sudo apt-get install -y curl && curl -sL https://deb.nodesource.com/setup_12.x | sudo bash -
+sudo apt-get install -y build-essential mongodb-org nodejs fontconfig graphicsmagick
+sudo npm install -g inherits n && sudo n 12.18.4
+curl -L https://releases.rocket.chat/latest/download -o /tmp/rocket.chat.tgz
+tar -xzf /tmp/rocket.chat.tgz -C /tmp
+cd /tmp/bundle/programs/server && npm install
+sudo mv /tmp/bundle /opt/Rocket.Chat
+sudo useradd -M rocketchat && sudo usermod -L rocketchat
+sudo chown -R rocketchat:rocketchat /opt/Rocket.Chat
