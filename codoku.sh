b235b2075b19a69698b15011633d878a398b99b6
Briggsdf <briggsdf@gmail.com>
2021-10-12 16:52:47 -0400

    Update codoku.sh

--- codoku.sh
+++ codoku.sh
@@ -1,12 +1,5 @@
-wget -nv -O - https://get.docker.com/ | sh
-wget -nv -O - https://packagecloud.io/dokku/dokku/gpgkey | apt-key add -
-export SOURCE="https://packagecloud.io/dokku/dokku/ubuntu/"
-export OS_ID="$(lsb_release -cs 2>/dev/null || echo "bionic")"
-echo "bionic focal" | grep -q "$OS_ID" || OS_ID="bionic"
-echo "deb $SOURCE $OS_ID main" | tee /etc/apt/sources.list.d/dokku.list
-apt-get update
-install dokku
-apt-get install dokku
+wget https://raw.githubusercontent.com/dokku/dokku/v0.25.7/bootstrap.sh
+sudo DOKKU_TAG=v0.25.7 bash bootstrap.sh
 sudo dokku plugin:install-dependencies --core 
 sudo dokku plugin:install https://github.com/dokku/dokku-postgres.git postgres
 sudo dokku plugin:install https://github.com/dokku/dokku-mongo.git mongo
